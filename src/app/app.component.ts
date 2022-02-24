0a4d2c97a9094581b12ca3a297c2aa17528018da
Angel Romero <angel@froged.com>
2022-02-24 10:16:32 +0100

    Simple example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component }       from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { FrogedService }   from './froged.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-froged';
+
+    public currentUser: any = null;
+
+    constructor(
+        private frogedService: FrogedService,
+    ) { }
+
+    loginAsPedro(): void {
+        this.currentUser = { userId: 'user_pedro', name: 'Pedro', email: 'user_pedro@ejemplo.com' };
+        this.frogedService.set({ userId: 'user_pedro', name: 'Pedro', email: 'user_pedro@ejemplo.com' });
+    }
+
+    loginAsCarmen(): void {
+        this.currentUser = { userId: 'user_pedro', name: 'Pedro', email: 'user_pedro@ejemplo.com' };
+        this.frogedService.set({ userId: 'user_carmen', name: 'Carmen', email: 'user_carmen@ejemplo.com' });
+    }
+
+    logout(): void {
+        this.currentUser = null;
+        this.frogedService.logout();
+    }
+
 }
